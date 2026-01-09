e5bb547247126605ad049735341d40b65caa08ed
jaydevramanandi <jaydev.ramanandi@gmail.com>
2026-01-09 12:02:57 +0530

    Basic setup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Load authentication routes (login/register/logout)
+require __DIR__.'/auth.php';
